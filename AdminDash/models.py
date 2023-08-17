3fd92b1f08d636c8eeb64a8578a12b3da70c7b5b
Onyinye Liveth <onyiorajiaku1@gmail.com>
2023-08-17 15:26:30 +0100

    models and templates models and templates

--- AdminDash/models.py
+++ AdminDash/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+class Admin(AbstractUser):
+    name = models.CharField(max_length=100, null=True)
+    email = models.EmailField(unique=True, null=True)
+    adminID = models.IntegerField(unique=True, null=True, primary_key=True)
+    bio = models.TextField(null=True, blank=True)
+    description = models.TextField(max_length=250, blank=True)
+    is_professional = models.BooleanField(default=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+
