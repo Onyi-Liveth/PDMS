7686df367ac6ea9d526cec6bc85bbb19d1d284a5
Onyinye Liveth <onyiorajiaku1@gmail.com>
2023-08-18 14:27:27 +0100

    models

--- AdminDash/models.py
+++ AdminDash/models.py
@@ -1,34 +1,40 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
+department = [
+    ('SR', 'Surgeon'),
+    ('NS', 'Nurse'),
+    ('PD', 'Pedeatrician'),
+    ('DR', 'Doctor'),
+    ('PS', 'Psychaitrist'),
+]
 
 # Create your models here.
-class Admin(AbstractUser):
-    name = models.CharField(max_length=100, null=False)
-    email = models.EmailField(max_length=100, unique=True, null=False, blank=False)
-    adminID = models.IntegerField(max_length=10,unique=True, null=False, primary_key=True)
-    dob = models.DateField(auto_now=True)
-    phone = PhoneField(blank=False, help_text='Contact phone number')
-    address = models.Charfield(max_length=250, null=False, blank=False)
-    age = models.IntegerField(max_length=3, null=False, blank=False)
-    bio = models.TextField(max_length=250, null=False, blank=False)
     
-    
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
-
 class Physician(models.Model):
-    doc_name = models.CharField(max_length=100, null=False, blank=False)
-    doc_email = models.EmailField(unique=True, null=False, blank=False)
-    doc_ID = models.CharField(unique=True, null=False, primary_key=True)
-    doc_dob = models.DateTimeField(auto_now=True, blank=False)
-    doc_age = models.IntegerField(null=False, max_length=3)
-    doc_phone = PhoneField(blank=False, help_text='Contact phone number')
-    doc_address = models.Charfield(max_length=250, null=False, blank=False)
-    doc_bio = models.TextField(null=False, blank=False)
-    doc_description = models.TextField(max_length=250, blank=True)
+    SR = 'Surgeon'
+    NS = 'Nurse'
+    PD = 'Pedeatrician'
+    DR = 'Doctor'
+    PS = 'Psychaitrist'
+    department = [
+    ('SR', 'Surgeon'),
+    ('NS', 'Nurse'),
+    ('PD', 'Pedeatrician'),
+    ('DR', 'Doctor'),
+    ('PS', 'Psychaitrist'),
+    ]
+
+    name = models.CharField(max_length=100, null=False, blank=True)
+    email = models.EmailField(unique=True, null=False, blank=True)
+    age = models.IntegerField(null=False, max_length=3)
+    phone = PhoneField(null = False, help_text='Contact phone number')
+    address = models.Charfield(max_length=250, null=False)
+    bio = models.TextField(null=False,max_length=250)
+    department = models.CharField(max_length=25, default= DR, choices=department)
+
+    def __str__(self):
+        return str(self.name)
 
 
-    USERNAME_FIELD = 'doc_email'
-    REQUIRED_FIELDS = ['username']
+
+    
--- patientDash/models.py
+++ patientDash/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 
 # Create your models here.
-patientID = models.CharField(unique=True, null=False, primary_key=True)
-pat_name = models.CharField(max_length=100, null=False)
-pat_email = models.EmailField(unique=True, null=False)
-pat_number = PhoneField(blank=False, help_text='Contact phone number')
-pat_gender = models.BooleanField(default=True)
-pat_DOB = models.DateTimeField(auto_now=True)
-pat_address = models.Charfield(max_length=250, null=False)
-next_of_kin = models.CharField(max_length=100, null=False)
-pat_age = models.IntegerField(null=False, max_length=3)
-occupation = models.TextField(blank=False, max_length=50 )
+class Patient(models.Model):
+  pat_name = models.CharField(max_length=100, null=True)
+  pat_email = models.EmailField(unique=True, null=True)
+  pat_number = PhoneField(blank=False, help_text='Contact phone number')
+  pat_gender = models.BooleanField(default=True)
+  pat_DOB = models.DateField(auto_now=True)
+  pat_address = models.Charfield(max_length=250, null=True)
+  next_of_kin = models.CharField(max_length=100, null=True)
+  pat_age = models.IntegerField(null=True, max_length=3)
+  occupation = models.TextField(max_length=50)
+  reg_date = models.DateField(auto_now=True)
+  
+
+  @property
+  def patientID(self):
+    return str(self.pat_name)
+
